87a3d269bc8d023a04ece99964913def30d020e6
JJohnson <Johnson@take-e-way.de>
2025-12-02 15:47:42 +0100

    ---

--- src/utils/processPassphrase.ts
+++ src/utils/processPassphrase.ts
@@ -20,3 +20,35 @@ export function processPassphrase(data: string): number {
 
   return result;
 }
+
+
+export function processExtendedPassphrase(data: string): number {
+  let value: number = 50;
+  let result: number = 0;
+  
+  data.split('\n').forEach(line => {
+    const prefix = line.slice(0, 1);
+    const suffix: number = Number(line.slice(1).trim());
+    
+    // Jeden Zwischenschritt prüfen
+    if (prefix === 'R') {
+      for (let i = 0; i < suffix; i++) {
+        value = (value + 1) % 100; //GRRR
+        if (value === 0) {
+          result += 1;
+        }
+      }
+    }
+    else {
+      for (let i = 0; i < suffix; i++) {
+        value = ((value - 1) % 100 + 100) % 100; //GRRR
+        if (value === 0) {
+          result += 1;
+        }
+      }
+    }
+
+  });
+
+  return result;
+}
